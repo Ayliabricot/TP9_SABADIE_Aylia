cc7a9200426c9e8069e09803e2f086f97e0be257
Aylia <aylia.sabadie@student.junia.com>
2025-03-23 17:01:40 +0100

    Affichage de tous les élèves.

--- index.php
+++ index.php
@@ -3,6 +3,19 @@
     
     require_once("./Model/pdo.php");
 
+    $resultat = $dbPDO->prepare("SELECT * FROM etudiants");
+    $resultat->execute();
+
+    $etudiants=$resultat->fetchAll();
+
+    echo "<ul>";
+
+    foreach ($etudiants as $etudiant){
+      echo "<li>".$etudiant["nom"]." ".$etudiant["prenom"]."</li>";
+    }
+
+    echo "</ul>";
+
     $content = ob_get_clean();
     require("./../inc/template.php");
 
