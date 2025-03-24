e3aa32ea7053023babbb9a88fb2f09de3e4a1448
Aylia <aylia.sabadie@student.junia.com>
2025-03-25 00:42:40 +0100

    J'ai préparé le formulaire avec des valeurs par défaut modifiables, je m'occuperai demain de la requête.

--- Views/modif_etudiant.php
+++ Views/modif_etudiant.php
@@ -3,12 +3,17 @@
     
     require_once("../Model/pdo.php");
 
-    
+    $id=$_GET["id"];
 
 ?>
 
+<h3>Nouvel étudiant : </h3>
 <form action="../index.php" method="post">
-      <input type="submit" value="Valider" />
+    
+  Prénom : <input type="text" name="prenom" value="<?= $_POST['prenom'] ?? 'Jean' ?>" />
+  Nom : <input type="text" name="nom" value="<?= $_POST['nom'] ?? 'Sérien' ?>" />
+    
+  <input type="submit" value="Valider" />
 </form>
 
 <?php $content = ob_get_clean(); ?>
