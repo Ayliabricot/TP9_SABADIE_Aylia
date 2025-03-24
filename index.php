dc8cbf78e3fe01787086a5292ebfdcaa539968ed
Aylia <aylia.sabadie@student.junia.com>
2025-03-24 20:28:30 +0100

    Affichage simultané des professeurs/étudiants avec amélioration de la mise-en-page.

--- index.php
+++ index.php
@@ -3,12 +3,25 @@
     
     require_once("./Model/pdo.php");
 
+    $resultat = $dbPDO->prepare("SELECT * FROM etudiants INNER JOIN classes ON etudiants.classe_id=classes.id");
+    $resultat->execute();
+
+    $etudiants=$resultat->fetchAll();
+
+    echo "<br><h3>Les élèves :</h3><ul>";
+
+    foreach ($etudiants as $etudiant){
+      echo "<li>".$etudiant["prenom"]." ".$etudiant["nom"]." ".$etudiant["libelle"]."</li>";
+    }
+
+    echo "</ul>";
+
     $resultat = $dbPDO->prepare("SELECT * FROM professeurs INNER JOIN matiere ON professeurs.id_matiere=matiere.id INNER JOIN classes ON professeurs.id_classe=classes.id");
     $resultat->execute();
 
     $professeurs=$resultat->fetchAll();
 
-    echo "<ul>";
+    echo "<h3>Les professeurs :</h3><ul>";
 
     foreach ($professeurs as $professeur){
       echo "<li>".$professeur["prenom"]." ".$professeur["nom"]." ".$professeur["lib"]." ".$professeur["libelle"]."</li>";
@@ -19,6 +32,7 @@
 
 ?>
 
+<h3>Nouvelle matière : </h3>
 <form action="./Views/nouvelle_matiere.php" method="post">
         
       Libelle : <input type="text" name="libelle" />
@@ -26,6 +40,7 @@
       <input type="submit" value="Valider" />
 </form>
 
+<h3>Nouvel étudiant : </h3>
 <form action="./Views/nouvel_etudiant.php" method="post">
         
       Prénom : <input type="text" name="prenom" />
