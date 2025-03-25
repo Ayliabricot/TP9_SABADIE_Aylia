55e876e8ddd02c36b85bc97159daef6867ba39a6
Aylia <aylia.sabadie@student.junia.com>
2025-03-25 15:22:33 +0100

    Changement des valeurs par défaut du questionnaire (j'avais pas compris qu'il fallait juste mettre les valeurs qu'on avait déjà).

--- Views/modif_etudiant.php
+++ Views/modif_etudiant.php
@@ -4,14 +4,22 @@
     require_once("../Model/pdo.php");
 
     $id=$_GET["id"];
+    $resultat = $dbPDO->prepare("SELECT * FROM etudiants WHERE id=:id");
+    $resultat->execute([
+      "id"=>$_GET["id"]
+    ]);
 
+    $etudiants=$resultat->fetchAll();
+    foreach($etudiants as $etudiant){
+      $modifier=$etudiant;
+    }
 ?>
 
 <h3>Nouvel étudiant : </h3>
 <form action="../index.php" method="post">
     
-  Prénom : <input type="text" name="prenom" value="<?= $_POST['prenom'] ?? 'Jean' ?>" />
-  Nom : <input type="text" name="nom" value="<?= $_POST['nom'] ?? 'Sérien' ?>" />
+  Prénom : <input type="text" name="prenom" value="<?= $_POST['prenom'] ?? $modifier['prenom'] ?>" />
+  Nom : <input type="text" name="nom" value="<?= $_POST['nom'] ?? $modifier['nom'] ?>" />
     
   <input type="submit" value="Valider" />
 </form>
